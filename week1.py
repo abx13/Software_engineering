6ad7c5e332e2f51099eb58ea7093a429778551ee
anna <anna.barraque@gmail.com>
2019-08-16 10:45:19 +0800

    python files

---
+++ week1.py
@@ -0,0 +1,11 @@
+print("cat")
+1 << 8
+"Hello" + "world"
+print ("😼")
+%%bash 
+echo "print (3 + 5)" > week1.py ## This is equivalent to write 'print (3 + 5)' inside a file.
+cat week1.py                    ## The command 'cat' write on the screen wat is in a file.
+3 + 8
+x = print
+print(x)
+def addition (a="2" , b="2"): return a + b
